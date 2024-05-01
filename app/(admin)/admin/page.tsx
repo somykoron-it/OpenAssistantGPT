e9a0dfcb3dff3f2836637ac41225bf02a731f0fa
somykoron-it <138964080+somykoron-it@users.noreply.github.com>
2024-05-01 16:33:18 +0600

    Fixed the overlay issue

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -70,12 +70,12 @@ const AdminDashboard: React.FC = () => {
   };
 
   const changePassword = async () => {
-    if(!oldPassword || !newPassword){
+    if (!oldPassword || !newPassword) {
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
         text: 'Field should not empty',
-      })
+      });
       return;
     }
     try {
@@ -219,6 +219,7 @@ const AdminDashboard: React.FC = () => {
       const correctPassword = data.password;
 
       if (password === correctPassword) {
+        fetchUserList();
         setIsLoggedIn(true);
         setShowPopup(false);
         localStorage.setItem('adminPassword', password);
@@ -298,211 +299,213 @@ const AdminDashboard: React.FC = () => {
     }
   };
 
-  useEffect(() => {
-    // Call fetchData function  the component mounts
-    fetchUserList();
-  }, []);
+  // useEffect(() => {
+  //   // Call fetchData function  the component mounts
+  //   fetchUserList();
+  // }, []);
 
   return (
     <div className="bg-[#000000] min-h-screen  w-fit lg:w-full">
       {showOverlay && !isLoggedIn && <Overlay />}
-      <div className="relative">
-        <div className="p-6 flex flex-col gap-y-10">
-          <div className="flex justify-between mb-4">
-            <div className="flex items-center gap-x-4">
-              <h1 className="text-[#ffffff] text-[25px] font-semibold">
-                Users
-              </h1>
-              <div className="relative text-[#ffffff]">
-                <input
-                  type="text"
-                  placeholder="Search..."
-                  className="w-[300px] px-4 py-[8px] pr-10 rounded-full bg-[#1E1D2D] placeholder:text-[#ffffff] text-[14px]"
-                  value={searchQuery}
-                  onChange={handleSearch}
-                />
-                <Icons.search className="h-4 w-4 absolute right-4 top-1/2 transform -translate-y-1/2" />
+      {isLoggedIn && (
+        <div className="relative">
+          <div className="p-6 flex flex-col gap-y-10">
+            <div className="flex justify-between mb-4">
+              <div className="flex items-center gap-x-4">
+                <h1 className="text-[#ffffff] text-[25px] font-semibold">
+                  Users
+                </h1>
+                <div className="relative text-[#ffffff]">
+                  <input
+                    type="text"
+                    placeholder="Search..."
+                    className="w-[300px] px-4 py-[8px] pr-10 rounded-full bg-[#1E1D2D] placeholder:text-[#ffffff] text-[14px]"
+                    value={searchQuery}
+                    onChange={handleSearch}
+                  />
+                  <Icons.search className="h-4 w-4 absolute right-4 top-1/2 transform -translate-y-1/2" />
+                </div>
+              </div>
+              <div className="flex gap-[10px]">
+                <button
+                  className="bg-[#0d3a60] text-white p-2 rounded"
+                  onClick={() => setshowPasswordChangePopup(true)}
+                >
+                  Change Password
+                </button>
+                <button
+                  className="bg-[#5151EB] text-white p-2 rounded"
+                  onClick={togglePopup}
+                >
+                  Generate Link
+                </button>
               </div>
             </div>
-            <div className="flex gap-[10px]">
-              <button
-                className="bg-[#0d3a60] text-white p-2 rounded"
-                onClick={() => setshowPasswordChangePopup(true)}
-              >
-                Change Password
-              </button>
-              <button
-                className="bg-[#5151EB] text-white p-2 rounded"
-                onClick={togglePopup}
-              >
-                Generate Link
-              </button>
-            </div>
-          </div>
 
-          {showPopup && (
-            <div
-              className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[999]"
-              onClick={handleOutsideClick}
-            >
-              <div className="text-white bg-[#343341] w-auto h-auto p-6 rounded shadow-lg relative">
-                <button
-                  className="absolute top-2 right-2"
-                  onClick={() => setShowPopup(false)}
-                >
-                  &times;
-                </button>
-                <h2 className="text-lg mb-4">Generate Link</h2>
+            {showPopup && (
+              <div
+                className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[999]"
+                onClick={handleOutsideClick}
+              >
+                <div className="text-white bg-[#343341] w-auto h-auto p-6 rounded shadow-lg relative">
+                  <button
+                    className="absolute top-2 right-2"
+                    onClick={() => setShowPopup(false)}
+                  >
+                    &times;
+                  </button>
+                  <h2 className="text-lg mb-4">Generate Link</h2>
 
-                {loading ? (
-                  <div className="flex justify-center items-center w-full">
-                    <Icons.spinner className="mr-2 h-8 w-8 animate-spin" />
-                  </div>
-                ) : (
-                  <div className="flex h-[80%] gap-x-2 justify-center items-center">
-                    <div className="w-full border-2 border-blue-300 p-2 rounded-lg">
-                      <p>{magicLink}</p>
+                  {loading ? (
+                    <div className="flex justify-center items-center w-full">
+                      <Icons.spinner className="mr-2 h-8 w-8 animate-spin" />
                     </div>
+                  ) : (
+                    <div className="flex h-[80%] gap-x-2 justify-center items-center">
+                      <div className="w-full border-2 border-blue-300 p-2 rounded-lg">
+                        <p>{magicLink}</p>
+                      </div>
 
+                      <button
+                        className="bg-[#646ce0] text-white py-2 px-4 rounded"
+                        onClick={handleCopyToClipboard}
+                      >
+                        <Icons.copy />
+                      </button>
+                    </div>
+                  )}
+                </div>
+              </div>
+            )}
+
+            {showPasswordChangePopup && (
+              <div
+                className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[999]"
+                onClick={handleOutsideClick}
+              >
+                <div className="text-white bg-[#343341] w-auto h-auto p-6 rounded shadow-lg relative">
+                  <button
+                    className="absolute top-2 right-2"
+                    onClick={() => setshowPasswordChangePopup(false)}
+                  >
+                    &times;
+                  </button>
+                  <h2 className="text-lg mb-4">Change Password</h2>
+
+                  <div className="flex flex-col gap-y-4">
+                    <input
+                      type="password"
+                      placeholder="Old Password"
+                      className="border border-gray-300 rounded p-2"
+                      value={oldPassword}
+                      onChange={(e) => setOldPassword(e.target.value)}
+                    />
+                    <input
+                      type="password"
+                      placeholder="New Password"
+                      className="border border-gray-300 rounded p-2"
+                      value={newPassword}
+                      onChange={(e) => setNewPassword(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="flex h-[80%] gap-x-2 justify-center items-center">
                     <button
-                      className="bg-[#646ce0] text-white py-2 px-4 rounded"
-                      onClick={handleCopyToClipboard}
+                      className="bg-[#646ce0] text-white mt-[10px] py-2 px-4 rounded w-[100%]"
+                      onClick={changePassword}
+                      disabled={loading}
                     >
-                      <Icons.copy />
+                      {loading ? (
+                        <div className="flex justify-center">
+                          <Icons.spinner className="h-5 w-5 animate-spin" />
+                        </div>
+                      ) : (
+                        <span>Submit</span>
+                      )}
                     </button>
                   </div>
-                )}
-              </div>
-            </div>
-          )}
-
-          {showPasswordChangePopup && (
-            <div
-              className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[999]"
-              onClick={handleOutsideClick}
-            >
-              <div className="text-white bg-[#343341] w-auto h-auto p-6 rounded shadow-lg relative">
-                <button
-                  className="absolute top-2 right-2"
-                  onClick={() => setshowPasswordChangePopup(false)}
-                >
-                  &times;
-                </button>
-                <h2 className="text-lg mb-4">Change Password</h2>
-
-                <div className="flex flex-col gap-y-4">
-                  <input
-                    type="password"
-                    placeholder="Old Password"
-                    className="border border-gray-300 rounded p-2"
-                    value={oldPassword}
-                    onChange={(e) => setOldPassword(e.target.value)}
-                  />
-                  <input
-                    type="password"
-                    placeholder="New Password"
-                    className="border border-gray-300 rounded p-2"
-                    value={newPassword}
-                    onChange={(e) => setNewPassword(e.target.value)}
-                  />
-                </div>
-
-                <div className="flex h-[80%] gap-x-2 justify-center items-center">
-                  <button
-                    className="bg-[#646ce0] text-white mt-[10px] py-2 px-4 rounded w-[100%]"
-                    onClick={changePassword}
-                    disabled={loading}
-                  >
-                    {loading ? (
-                      <div className="flex justify-center">
-                        <Icons.spinner className="h-5 w-5 animate-spin" />
-                      </div>
-                    ) : (
-                      <span>Submit</span>
-                    )}
-                  </button>
                 </div>
               </div>
-            </div>
-          )}
-          <div className="bg-[#1E1D2D] h-auto xl:overflow-y-hidden rounded-lg p-6 text-[#ffffff]">
-            <table className="min-w-full border-b border-[#272536]">
-              <thead className="p-4">
-                <tr className="bg-[#343341] rounded-lg text-left">
-                  {columns.map((column) => (
-                    <th key={column} className="p-[12px]">
-                      {column}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {paginatedItems.length === 0 ? (
-                  <tr>
-                    <td colSpan={columns.length} className="text-center py-4">
-                      <Icons.user className="text-center flex justify-center items-center w-full" />{' '}
-                      No users found.
-                    </td>
+            )}
+            <div className="bg-[#1E1D2D] h-auto xl:overflow-y-hidden rounded-lg p-6 text-[#ffffff]">
+              <table className="min-w-full border-b border-[#272536]">
+                <thead className="p-4">
+                  <tr className="bg-[#343341] rounded-lg text-left">
+                    {columns.map((column) => (
+                      <th key={column} className="p-[12px]">
+                        {column}
+                      </th>
+                    ))}
                   </tr>
-                ) : (
-                  paginatedItems.map((item) => (
-                    <tr
-                      key={item.id}
-                      className="hover:bg-[#252434] rounded-[24px] h-[60px]"
-                    >
-                      <TableCell>{item.registrationId}</TableCell>
-                      <TableCell>{item.name}</TableCell>
-                      <TableCell>{item.email}</TableCell>
-                      <TableCell>
-                        <Switch
-                          className="data-[state=unchecked]:bg-[#0F172A] data-[state=checked]:bg-[#646ce0]"
-                          checked={item.hasAccess}
-                          onCheckedChange={() =>
-                            handleToggleProperty(
-                              item.email,
-                              item.hasAccess,
-                              'premium'
-                            )
-                          }
-                        />
-                      </TableCell>
-                      <TableCell>
-                        <Switch
-                          className="data-[state=unchecked]:bg-[#0F172A] data-[state=checked]:bg-[#25B55B]"
-                          checked={item.isActive}
-                          onCheckedChange={() =>
-                            handleToggleProperty(
-                              item.email,
-                              item.isActive,
-                              'active'
-                            )
-                          }
-                        />
-                      </TableCell>
+                </thead>
+                <tbody>
+                  {paginatedItems.length === 0 ? (
+                    <tr>
+                      <td colSpan={columns.length} className="text-center py-4">
+                        <Icons.user className="text-center flex justify-center items-center w-full" />{' '}
+                        No users found.
+                      </td>
                     </tr>
-                  ))
-                )}
-              </tbody>
-            </table>
+                  ) : (
+                    paginatedItems.map((item) => (
+                      <tr
+                        key={item.id}
+                        className="hover:bg-[#252434] rounded-[24px] h-[60px]"
+                      >
+                        <TableCell>{item.registrationId}</TableCell>
+                        <TableCell>{item.name}</TableCell>
+                        <TableCell>{item.email}</TableCell>
+                        <TableCell>
+                          <Switch
+                            className="data-[state=unchecked]:bg-[#0F172A] data-[state=checked]:bg-[#646ce0]"
+                            checked={item.hasAccess}
+                            onCheckedChange={() =>
+                              handleToggleProperty(
+                                item.email,
+                                item.hasAccess,
+                                'premium'
+                              )
+                            }
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <Switch
+                            className="data-[state=unchecked]:bg-[#0F172A] data-[state=checked]:bg-[#25B55B]"
+                            checked={item.isActive}
+                            onCheckedChange={() =>
+                              handleToggleProperty(
+                                item.email,
+                                item.isActive,
+                                'active'
+                              )
+                            }
+                          />
+                        </TableCell>
+                      </tr>
+                    ))
+                  )}
+                </tbody>
+              </table>
 
-            <div className="flex justify-center mt-4">
-              {Array.from({ length: totalPages }, (_, index) => (
-                <button
-                  key={index + 1}
-                  className={`mx-2 px-4 rounded ${
-                    currentPage === index + 1
-                      ? 'bg-[#524DFE] text-white'
-                      : 'bg-[#000000]'
-                  }`}
-                  onClick={() => handlePageChange(index + 1)}
-                >
-                  {index + 1}
-                </button>
-              ))}
+              <div className="flex justify-center mt-4">
+                {Array.from({ length: totalPages }, (_, index) => (
+                  <button
+                    key={index + 1}
+                    className={`mx-2 px-4 rounded ${
+                      currentPage === index + 1
+                        ? 'bg-[#524DFE] text-white'
+                        : 'bg-[#000000]'
+                    }`}
+                    onClick={() => handlePageChange(index + 1)}
+                  >
+                    {index + 1}
+                  </button>
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
